1f43fc7abbbb045efe166d8dcbf1336ebad68b5b
Vicenzo <arturo97xd@gmail.com>
2023-02-20 05:40:37 -0500

    ez

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react'
+import { Blog, Footer, Possibility, Header, Features, WhatGPT4} from './containers';
+import { CTA, Navbar, Brand} from './components';
 import './App.css';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+const App = () => {
+    return (
+    <div className='App'>
+        <div className='gradient__bg' >
+            <Navbar /> 
+            <Header />
+        </div>
+        <Brand />
+        <WhatGPT4 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
+
     </div>
-  );
+)
 }
 
-export default App;
+export default App
